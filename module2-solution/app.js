cf5cd7bb3f2e3fd191a448e06bba5b2c667e3fe2
Lise Bilhaut <lise.bilhaut@gmail.com>
2019-06-19 08:57:54 -0400

    Update app.js

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -5,18 +5,69 @@
   angular.module('ShoppingList',[])
   .controller('ToBuyController', ToBuyController)
   .controller('AlreadyBoughtController',AlreadyBoughtController )
-  .service('ShoppingLisCheckOffService', ShoppingLisCheckOffService);
+  .service('ShoppingListCheckOffService', ShoppingLisCheckOffService);
   
-  ToBuyController.$inject=['ShoppingLisCheckOffService'];
-  function ToBuyController(ShoppingLisCheckOffService){
+  ToBuyController.$inject=['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService){
+    var toBuy = this;
+    
+    toBuy.toBuyList = ShoppingListCheckOffService.getToBuyItems();
   
-  }
+  
+  };
   
   
-  AlreadyBoughtController.$inject=['ShoppingLisCheckOffService'];
-  function AlreadyBoughtController(ShoppingLisCheckOffService){
+  AlreadyBoughtController.$inject=['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService){
+    var alreadyBought = this;
+    
+    alreadyBought.alreadyBoughtList = ShoppingListCheckOffService.getBoughtItems();
+  };
   
-  }
+  function ShoppingListCheckOffService(){
+    var service = this;
+    
+  var toBuyItems =
+    {
+     name: "Cookies",
+     quantity: 1
+    },
+    {
+     name: "Chips",
+     quantity: 2
+    },
+    {
+     name: "Houmous",
+     quantity: 3
+    },
+    {
+     name: "Carrot",
+     quantity: 4
+    },
+    {
+     name: "Drinks",
+     quantity: 5
+    }
+  ];
+    
+    var boughtItems =[];
+    
+    service.getToBuyItems = function(){
+      return toBuyItems;
+    };
+    
+    service.getBoughtItems = function(){
+      return boughtItems;
+    };
+    
+    service.updateShoppingList = function(itemIndex){
+      var item = toBuyItems[itemIndex];
+      toBuyItems.splice(itemIndex,1);
+      boughtItems.add(item);
+    };
+    
+    
+  };
   
   
 })();
