ebe8aee1a6ec9b68e03e9ffc53990a08cf30235b
Lise Bilhaut <lise.bilhaut@gmail.com>
2019-06-19 08:41:42 -0400

    Update app.js

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,1 +1,23 @@
+(function(){
+
+  'use strict';
+  
+  angular.module('ShoppingList',[])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController',AlreadyBoughtController )
+  .service('ShoppingLisCheckOffService', ShoppingLisCheckOffService);
+  
+  ToBuyController.$inject=['ShoppingLisCheckOffService'];
+  function ToBuyController(ShoppingLisCheckOffService){
+  
+  }
+  
+  
+  AlreadyBoughtController.$inject=['ShoppingLisCheckOffService'];
+  function AlreadyBoughtController(ShoppingLisCheckOffService){
+  
+  }
+  
+  
+})();
 
