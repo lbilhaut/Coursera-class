bcde086e3cd5eb1d4f6877a70db93afe46c318f2
Lise Bilhaut <lise.bilhaut@gmail.com>
2019-06-25 11:15:01 -0400

    Add files via upload

---
+++ module3-solution/app.js
@@ -0,0 +1,43 @@
+(function(){
+	'use strict';
+	
+	angular.module('NarrowItDownApp',[])
+	.controller('NarrowItDownController', NarrowItDownController)
+	.service('MenuSearchService', MenuSearchService);
+	
+	NarrowItDownController.$inject=['MenuSearchService'];
+	function NarrowItDownController(MenuSearchService){
+		var controllerInstance = this;
+		
+		MenuSearchService.getMenuItems();
+		
+	}
+	
+	MenuSearchService.$inject=['$http'];
+	function MenuSearchService($http){
+		
+		var service = this;
+		
+		console.log("In the MenuSearchService");
+		
+		service.getMenuItems = function(){
+			console.log("In the getMenuItems service");
+			var response = $http({
+				method: "GET",
+				url: "http://davids-restaurant.herokuapp.com/menu_items.json"
+			})
+			.then(function (response){
+				controllerInstance.items = response.data.menu_items;
+				console.log("items are " + items);
+			})
+			.catch(function(error){
+				console.log("Something went wrong");
+			});
+			console.log("Returning the response " + response);
+			return response;
+		};
+		
+	}
+	
+	
+})();
