f087342fcfb897043f59de8c53b274b250c37730
Lise Bilhaut <lise.bilhaut@gmail.com>
2019-06-25 16:04:51 -0400

    Add files via upload

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -6,43 +6,66 @@
 	.service('MenuSearchService', MenuSearchService)
 	.directive('foundItems', FoundItems);
 
-	
+
 	function FoundItems(){
-		var ddo = {
-				templateUrl: 'foundItems.html',
-//				scope:{
-//					name: '<',
-//				},
-//			controller: FoundItemsDirectiveController,
-//			controllerAs: 'list',
-//			bindToController: true
-			restrict: 'E'
-		};
-		console.log("In the directive");
-		return ddo;
-		
+	var ddo = {
+	templateUrl: 'foundItems.html',
+//	scope:{
+//	name: '<',
+//	},
+//	controller: FoundItemsDirectiveController,
+//	controllerAs: 'list',
+//	bindToController: true
+	restrict: 'E'
+	};
+	console.log("In the directive");
+	return ddo;
+
 	}
-	
-	
+
+
 //	function FoundItemsDirectiveController(){
-//		
+
 //	}
-	
-	NarrowItDownController.$inject=['MenuSearchService'];
-	function NarrowItDownController(MenuSearchService){
+
+	NarrowItDownController.$inject=['$scope','MenuSearchService'];
+	function NarrowItDownController($scope, MenuSearchService){
 		var menu = this;
 
-		menu.found = [];
-		
-		MenuSearchService.getMatchedMenuItems("chicken").then(
-				function (foundItems){
-					menu.found = foundItems;
-//					console.log("Found items after return " + foundItems[42].description);						
-					console.log("Found is " + menu.found[42].name);
-			}
-		);
-		
-	
+		menu.buttonActivated = function(){
+			console.log("Search term is " + $scope.searchWord);
+			console.log("Button activated!");
+			var promise = MenuSearchService.getMatchedMenuItems($scope.searchWord); 
+
+
+			promise.then( function (foundItems){
+				menu.found = foundItems;
+			})
+			.catch(function (error){
+				console.log("Something went wrong");
+			});
+
+		};
+
+//		var promise = MenuSearchService.getMatchedMenuItems($scope.searchWord); 
+
+
+
+
+//		menu.found = [];
+
+//		MenuSearchService.getMatchedMenuItems("chicken").then(
+//		function (foundItems){
+//		menu.found = foundItems;
+
+////		console.log("Found items after return " + foundItems[42].description);						
+//		console.log("Found is " + menu.found[42].name);
+//		return menu.found;
+//		}
+//		);
+
+//		console.log("Found is now" + menu.found[42].name);
+
 
 //		promise.then(function (response){
 //		console.log("Response is returned!");
@@ -61,7 +84,6 @@
 
 		var service = this;
 
-		console.log("In the MenuSearchService");
 
 //		service.getMenuItems = function(){
 //		console.log("In the getMenuItems service");
@@ -71,7 +93,6 @@
 //		});
 
 		service.getMatchedMenuItems = function(searchTerm){
-			console.log("In the getMenuItems service");
 			console.log("searchTerm is "  + searchTerm)
 			var foundItems =[];
 
@@ -80,9 +101,7 @@
 				url: "https://davids-restaurant.herokuapp.com/menu_items.json"
 			})
 			.then(function (response){
-				console.log("Response is returned!");
 				console.log("searchTerm is still "  + searchTerm);
-				console.log("Length of menu is " + response.data.menu_items.length);
 				for(var i = 0; i < response.data.menu_items.length; i++){
 					if(response.data.menu_items[i].description.toLowerCase().indexOf(searchTerm.toLowerCase()) !==-1){
 						console.log("search term found!");
@@ -94,16 +113,13 @@
 						foundItems.push(item);
 					}
 				}
-				console.log("Found items before return " + foundItems);
-				console.log("Length is " + foundItems.length);
-//				console.log("Item 4 " + foundItems[4].name);
 				return foundItems;
 
 			})
 			.catch(function (error){
 				console.log("Something went wrong");
 			});
-			
+
 		};
 
 	}
